4f9c4a40ed1c0b4a2a875938d35f9a497e67216f
lmericle <lmericle@mail.nih.gov>
2015-07-10 18:05:25 -0400

    remove unneeded files

--- def/proto1/config/app_settings.rb
+++
@@ -1,6 +0,0 @@
-original_verbose = $VERBOSE
-$VERBOSE = nil  # suppress warning
-# TBD - specify for your system
-PHR_SUPPORT_EMAIL = 'phrgeneric@mailinator.com'
-$VERBOSE = original_verbose
-ActiveSupport::Deprecation.silenced = false
